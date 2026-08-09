758a2b22927d98e354a2e6c170f150f667a8d114
AliDehghani <alidehghani8400@gmail.com>
2026-08-09 15:14:16 +0330

    Implement x0.5 processor

---
+++ systems/System with bus interface/filtered_half/Memory.h
@@ -0,0 +1,184 @@
+#include <systemc.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "Bus.h"
+
+
+template <int N>
+SC_MODULE(Memory)
+{
+	int DebugON = 0;
+	int Loading = 1;
+	int StartingLocation = 0;
+	int PuttingData = 0;
+	std::string file;
+
+	sc_in<sc_logic> clk;
+
+	sc_in<sc_logic> t_cs;    
+    sc_in<sc_lv<N>> t_addr; 
+    sc_in<sc_lv<N>> t_in; 
+    sc_out<sc_lv<N>> t_out;
+    sc_in<sc_logic> t_wr;
+    sc_in<sc_logic> t_rd;
+    sc_out<sc_logic> t_ready;
+
+	int memRange;
+	sc_lv<N> *mem;
+
+
+
+	SC_CTOR(Memory)
+	{
+
+		memRange = int(pow(2, N));
+		mem = new sc_lv<N>[memRange];
+
+		SC_THREAD(init);
+		SC_METHOD(readMem);
+		sensitive << t_addr << t_cs << t_rd;
+		SC_METHOD(writeMem);
+		sensitive << clk.pos()<< t_wr;
+		SC_THREAD(dump);
+		SC_METHOD(setMemReady);
+		sensitive << t_addr << t_cs << t_rd << t_wr;
+	}
+	void init() {
+		int i = 0;
+		sc_lv<N> data;
+		std::string str_data;
+
+		std::ifstream initFile("default_meminit.txt");
+	
+		std::ifstream initFileLoad;
+		std::ifstream PutAddr("addr.txt");
+		std::ifstream PutData("data.txt");
+		file = "mem.txt";
+		
+		if (Loading ) {
+
+			initFileLoad.open(file);
+			if (!initFileLoad.is_open()) {
+				std::cerr << "[ERROR] Cannot open file: " << file << std::endl;
+				return;
+			}
+
+			int count = 0;
+			while (getline(initFileLoad, str_data)) {
+				if (!str_data.empty()) {
+					if (count >= StartingLocation && i < memRange) {
+						data = sc_lv<N>(str_data.c_str());
+						mem[i++] = data;
+					}
+					count++;
+				}
+			}
+			initFileLoad.close();
+			if (DebugON) {
+				std::cout << "[MEM] Loaded memory from file: " << file << std::endl;
+			}
+		}
+
+		
+		else if (PuttingData) {
+			std::string addr_str, data_str;
+			while (getline(PutAddr, addr_str) && getline(PutData, data_str)) {
+				if (!addr_str.empty() && !data_str.empty()) {
+					int addr = std::stoi(addr_str);
+					if (addr < memRange) {
+						data = sc_lv<N>(data_str.c_str());
+						mem[addr] = data;
+						std::cout << "[MEM] OK. addr = " << addr <<" , data = " << data << std::endl;
+					}
+				}
+			}
+			PutAddr.close();
+			PutData.close();
+			if (DebugON) {
+				std::cout << "[MEM] Put data from addr.txt and data.txt" << std::endl;
+			}
+		}
+
+		else {
+			if (!initFile.is_open()) {
+				std::cerr << "[ERROR] Cannot open default init file!" << std::endl;
+				return;
+			}
+			while (getline(initFile, str_data)) {
+				if (!str_data.empty() && i < memRange) {
+					data = sc_lv<N>(str_data.c_str());
+					mem[i++] = data;
+				}
+			}
+			initFile.close();
+			if (DebugON) {
+				std::cout << "[MEM] Initialized memory from default file" << std::endl;
+			}
+		}
+	}
+
+
+	void readMem()
+	{
+		sc_lv<N> tempAdr;
+		tempAdr = t_addr;
+		t_out = 0;
+		if (t_cs == '1')
+		{
+			if (t_rd == '1')
+			{
+				if (tempAdr.to_uint() < memRange)
+				{
+					t_out = mem[tempAdr.to_uint()];
+				}
+			}
+		}
+	}
+	void writeMem()
+	{
+		sc_lv<N> tempAd;
+
+		if (t_cs == '1')
+		{
+			tempAd = t_addr;
+			if (tempAd.to_uint() < memRange)
+			{
+				if (t_wr == '1')
+				{
+					mem[tempAd.to_uint()] = t_in;
+					cout<< "[Memory] mem["<< tempAd.to_uint() << "] = "<< mem[tempAd.to_uint()] << endl;
+				}
+			}
+		}
+	}
+	void dump()
+	{
+		ofstream out;
+		wait(30, SC_NS);
+		out.open("dump.txt");
+		for (int i = 0; i < memRange; i++)
+		{
+			out << i << "\t" << mem[i] << endl;
+		}
+		out.close();
+	}
+	void setMemReady()
+	{
+		sc_lv<N> tempAd;
+		t_ready = SC_LOGIC_0;
+		// cout << "ready Ready is " << ready << "\n";
+		if (t_cs == '1')
+		{
+			tempAd = t_addr;
+			if (tempAd.to_uint() < memRange)
+			{
+				if (t_wr == '1' || t_rd == '1')
+				{
+					t_ready = SC_LOGIC_1;
+				}
+			}
+		}
+		
+	}
+};
---
+++ systems/System with bus interface/filtered_half/TB_Half.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <systemc.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <fstream>
+#include "System_Half.h"
+
+/*
+g++ TB_Half.cpp -I$HOME/systemc-3.0.2/install/include -L$HOME/systemc-3.0.2/install/lib \
+    -Wl,-rpath,$HOME/systemc-3.0.2/install/lib -lsystemc -o half_sim
+
+./half_sim > result_half.txt
+*/
+
+SC_MODULE(SystemTester)
+{
+    sc_signal<sc_logic> clk;
+
+    EmbeddedSystem<16> *systemModule;
+
+    SC_CTOR(SystemTester)
+    {
+        systemModule = new EmbeddedSystem<16>("systemModule");
+        (*systemModule)(clk);
+
+        SC_THREAD(clocking);
+    }
+
+    void clocking();
+};
+void SystemTester::clocking()
+{
+    clk = SC_LOGIC_0;
+    while (true)
+    {
+        wait(2.5, SC_NS);
+        clk = SC_LOGIC_0;
+        wait(2.5, SC_NS);
+        clk = SC_LOGIC_1;
+    }
+}
+
+int sc_main(int argc, char *argv[])
+{
+    SystemTester *TOP = new SystemTester("systemTest_TB");
+    TOP->systemModule->memory->Loading = 1;
+
+    // 44 chunks + a final 22050-sample extraction loop need far more
+    // simulated time than the Phase 3 smoke test did.
+    sc_start(20000000, SC_NS);
+
+    return 0;
+}
